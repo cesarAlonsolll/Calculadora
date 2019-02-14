677ae922781d5e48bd1b3dacdf41b58e62c5902e
cesarAlonsolll <43044721+cesarAlonsolll@users.noreply.github.com>
2019-02-14 11:39:45 -0500

    v 0.5

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,26 @@
 "use strict"
-function mouseSobreBoton(){
-    
+function alerta(evt){
+    let valor = evt.target.value;
+    console.log(cajaTexto.value)
+    switch(valor){
+        case "c":
+            cajaTexto.innerHTML = "0";
+            break;
+        case "=":
+            cajaTexto.innerHTML = eval(cajaTexto.innerHTML);
+            break;
+        default:
+            if(cajaTexto.innerHTML=="0"){
+                cajaTexto.innerHTML = valor;
+            }
+            else{
+                cajaTexto.innerHTML += valor;
+            }
+            break;
+    }
+}
+let botones = document.getElementsByTagName("button");
+let cajaTexto = document.getElementById("textoOperacion");
+for(let i = 0;i<botones.length;i++){
+    botones[i].onclick = alerta;
 }
